26adab1dc8fa300e9e7a860553c857ec60cdfc1e
Bowdenk <kevin.bowden12@gmail.com>
2023-12-11 11:05:52 +0000

    Counter Component

---
+++ frontend/src/components/Counter.js
@@ -0,0 +1,30 @@
+import {React,useState}  from "react";
+import Button from "./button";
+
+//Creates a counter component that takes a starting parameter, usually Zero.  Has buttons to handle increments.
+
+const Counter = ({count}) => {
+    const [orderCount, setOrderCount] = useState(count)
+
+    //logic to limit order range 0-30
+    {if(orderCount < 0){setOrderCount(0)}}
+    {if(orderCount > 30){setOrderCount(30)}}
+
+    const handleDecrement = () => {        
+        setOrderCount(prevCount => prevCount - 1);
+    }
+
+    const handleIncrement = () => {
+        setOrderCount(prevCount => prevCount + 1);
+    }
+
+    return (
+        <div className="Controls">
+            <Button text='-' func={handleDecrement}/>
+            <h2>{orderCount}</h2>
+            <Button text='+' func={handleIncrement}/>
+        </div>
+    )
+}
+
+export default Counter;
