e099cac01d08727be63591fcd03924165a0ad098
JPMaxim <145019308+JPMaxim@users.noreply.github.com>
2023-12-11 12:05:58 +0000

    first API commit
    created a state for the api request query and a state for the json data
    Still need to figure out putting the API key in .env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,30 @@ import {Basket} from './components/Basket'
 function App() {
 
   const [basket, setBasket] = useState('item one')
+  const [calories, setCalories] = useState([])
+  const [ingredient, setIngredient] = useState('brisket')
+
+  // const API_KEY = process.env.REACT_APP_API_KEY
+  
+  const fetchCalories = async (ingredient) => {
+    let url = "https://api.api-ninjas.com/v1/nutrition?query=" + ingredient
+    console.log(url)
+    const response = await fetch(url , {
+      headers: {
+        "X-Api-Key": "I1ag09jAflT2yzafLPY2rg==uQsRfm7qfrirU7eq"
+      }
+    })
+    const data = await response.json()
+    setCalories(data)
+  }
+  fetchCalories(ingredient)
+
   return (
     <div className="App">
       <Basket 
         basket = {basket}
       />
+      <p>{`${ingredient} calories: ${calories[0].calories}`}</p>
     </div>
   )
 }
