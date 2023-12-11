b6c8e179c2282f904efb857ad0c0a3dde2375dee
shuks91 <arteh@live.co.uk>
2023-12-11 13:03:10 +0000

    added pages and routes frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
-import {useState, useEffect} from 'react'
-import {Basket} from './components/Basket'
+import { useState, useEffect } from 'react'
+import { Basket } from './components/Basket'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { MenuPage } from './pages/MenuPage'
+import { MenuItemPage } from './pages/MenuItemPage';
+import { CheckoutPage } from './pages/CheckoutPage';
 
 function App() {
 
@@ -9,11 +14,11 @@ function App() {
   const [ingredient, setIngredient] = useState('brisket')
 
   // const API_KEY = process.env.REACT_APP_API_KEY
-  
+
   const fetchCalories = async (ingredient) => {
     let url = "https://api.api-ninjas.com/v1/nutrition?query=" + ingredient
     console.log(url)
-    const response = await fetch(url , {
+    const response = await fetch(url, {
       headers: {
         "X-Api-Key": "I1ag09jAflT2yzafLPY2rg==uQsRfm7qfrirU7eq"
       }
@@ -25,17 +30,39 @@ function App() {
     fetchCalories(ingredient)
   }, [])
 
-  if(!calories || !basket) return (
+  if (!calories || !basket) return (
     <div>
       <h1>loading...</h1>
     </div>
   )
   return (
     <div className="App">
-      <Basket 
-        basket = {basket}
+      <Basket
+        basket={basket}
       />
       <p>{`${ingredient} calories: ${calories[0].calories}`}</p>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='/menu'
+            element={<MenuPage />}
+          />
+          <Route
+            path='/menuitem'
+            element={<MenuItemPage />}
+          />
+
+          <Route
+            path='/checkout'
+            element={<CheckoutPage />}
+          />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/pages/MenuItemPage.js
@@ -0,0 +1,7 @@
+export const MenuItemPage = () => {
+    return (
+        <div>
+            <h1>Menu Item</h1>
+        </div>
+    )
+}
